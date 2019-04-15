f8ba1e6559ee8ba5d28fdd72439a3becb6461bf4
root <cuiq14@chinaunicom.cn>
2019-04-15 21:34:45 +0800

    完成了视频1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Test myBatis");
 
-        System.out.println("更改");
-        System.out.println("TestGithub");
 
+        String resource="PersonMapper.xml";
+        Reader reader = Resources.getResourceAsReader("config.xml");
+
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        String statement ="PersonMapper.selectByName";
+        Person person =sqlSession.selectOne(statement,"cuiqiang");
+
+        System.out.println(person);
+        sqlSession.close();
 
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -27,4 +27,9 @@ public class Person {
     public void setScore(double score) {
         this.score = score;
     }
+
+ @Override
+    public String toString(){
+        return "name: "+name+"   age: "+age +"   Score:  "+score;
+ }
 }
